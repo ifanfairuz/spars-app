7b2c72fb4e0d648ea93e6b945dab58c62379d398
Ifan Fairuz <ifanfairuz@gmail.com>
2021-10-24 17:13:07 +0700

    [ADD] error exception

--- src/support/http/exception/index.ts
+++ src/support/http/exception/index.ts
@@ -1,7 +1,9 @@
 import ErrorException from "./ErrorException";
 import NetworkErrorException from "./NetworkErrorException";
+import UnauthErrorException from "./UnauthErrorException";
 
 export {
   ErrorException,
-  NetworkErrorException
+  NetworkErrorException,
+  UnauthErrorException
 }
